c3480a7ee1789fe15e45f93c17b7d55a1a714f14
renehernandez <renehr9102@protonmail.com>
2020-12-16 12:56:39 -0500

    Add destroy review app workflow

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,38 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site running on App Platform! <strong>This site was built within a Docker container!</strong></p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+var IndexPage; 
+
+if (process.env.REVIEW_ENVIRONMENT == 'true') {
+  IndexPage = () => (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site running on App Platform! <strong>This site was built within a Docker container!</strong></p>
+      <p>This is a review deployment</p>
+      <p>Now go build something great on your branch.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
+else {
+  IndexPage = () => (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site running on App Platform! <strong>This site was built within a Docker container!</strong></p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
